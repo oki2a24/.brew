25548f27a63a19e6afc4e8d59038d5dfbb63cba4
oki2a24 <oki2a24@gmail.com>
2019-01-23 07:19:21 +0900

    feat: インストールするアプリを複数追加する

--- install_app.sh
+++ install_app.sh
@@ -14,5 +14,11 @@ readonly TOOLS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 brew update && brew upgrade && brew cleanup
 
+brew cask install google-chrome
+brew cask install docker
+brew cask install evernote
+brew cask install virtualbox
 brew install git
 bash "$TOOLS_DIR/set_git_completion.sh"
+brew install packer
+brew install vim
