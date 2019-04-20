aa1ff72a531860dc0fe51862f1d2cc29392231c7
oki2a24 <oki2a24@gmail.com>
2019-04-20 16:35:29 +0900

    feat: Brewfile を使った管理に切り替える

---
+++ Brewfile
@@ -0,0 +1,32 @@
+tap "caskroom/cask"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "kryptco/tap"
+tap "universal-ctags/universal-ctags"
+brew "bash-completion"
+brew "composer"
+brew "fzf"
+brew "git"
+brew "python"
+brew "mas"
+brew "packer"
+brew "php"
+brew "the_silver_searcher"
+brew "tmux"
+brew "tree"
+brew "vim"
+brew "kryptco/tap/kr"
+brew "universal-ctags/universal-ctags/universal-ctags", args: ["HEAD"]
+cask "docker"
+cask "google-chrome"
+cask "virtualbox"
+cask "visual-studio-code"
+mas "", id: 409183694
+mas "com.amazon.Kindle", id: 405399194
+mas "com.apple.garageband10", id: 682658836
+mas "com.apple.iMovieApp", id: 408981434
+mas "com.apple.iWork.Numbers", id: 409203825
+mas "com.apple.iWork.Pages", id: 409201541
+mas "com.evernote.Evernote", id: 406056744
+mas "com.lastpass.LastPass", id: 926036361
--- install_app.sh
+++ install_app.sh
@@ -12,38 +12,9 @@ set -eux
 
 readonly TOOLS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-brew update && brew upgrade && brew cleanup
-
-# Homebrew Cask http://caskroom.io/
-brew tap caskroom/cask
-brew cask upgrade
-brew cask install google-chrome
-brew cask install docker
-bash "${TOOLS_DIR}/set_docker_bash_completion.sh"
-brew cask install virtualbox
-brew cask install visual-studio-code
-
-brew install bash-completion
+brew bundle install
 bash "${TOOLS_DIR}/set_bash_completion.sh"
-brew install composer
 bash "${TOOLS_DIR}/set_composer.sh"
-brew install fzf
+bash "${TOOLS_DIR}/set_docker_bash_completion.sh"
 bash "${TOOLS_DIR}/set_fzf.sh"
-brew install git
 bash "${TOOLS_DIR}/set_git_completion.sh"
-brew install kryptco/tap/kr
-brew install mas
-brew install packer
-brew install php
-brew install the_silver_searcher
-brew install tmux
-brew install tree
-brew install vim
-
-brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
-
-mas upgrade
-mas install 405399194 # Kindle
-mas install 406056744 # Evernote
-mas install 926036361 # LastPass
